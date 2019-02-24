25488d0d80d6d799f004d91b93f9c78af05e32b0
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2019-02-24 20:23:40 +0200

    Use junit 5 testing

--- selection_sort/src/main/java/com/ahmedsameha1/edx_algorithms_and_data_structures/selection_sort/SelectionSort.java
+++ selection_sort/src/main/java/com/ahmedsameha1/edx_algorithms_and_data_structures/selection_sort/SelectionSort.java
@@ -2,9 +2,8 @@ package com.ahmedsameha1.edx_algorithms_and_data_structures.selection_sort;
 
 import java.util.Arrays;
 
-public class selection_sort {
-    public static void main(String[] args) {
-        int[] intArr = {4,9,7,1,3,6,5,-1, 33, -10, 77, 0};
+public class SelectionSort {
+    public int[] sort(int[] intArr) {
         for (int i = 0; i < intArr.length; i++) {
             int min = i;
             for (int j = i + 1; j < intArr.length; j++) {
@@ -15,6 +14,6 @@ public class selection_sort {
                 }
             }
         }
-        System.out.println(Arrays.toString(intArr));
+        return intArr;
     }
 }
---
+++ selection_sort/src/test/java/com/ahmedsameha1/edx_algorithms_and_data_structures/selection_sort/TestSelectionSort.java
@@ -0,0 +1,15 @@
+package com.ahmedsameha1.edx_algorithms_and_data_structures.selection_sort;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TestSelectionSort {
+    private SelectionSort selectionSort = new SelectionSort();
+
+    @Test
+    public void test() {
+        assertArrayEquals(new int[] {1, 2, 3, 4}, selectionSort.sort(new int[] { 4, 3, 2, 1}));
+        assertArrayEquals(new int[] {-1, 1, 2, 3, 4}, selectionSort.sort(new int[] { 4, 3, 2, 1, -1}));
+        assertArrayEquals(new int[] {-1, 15, 15, 52, 73, 104}, selectionSort.sort(new int[] { 15, 73, 104, 15, 52, -1}));
+    }
+}
