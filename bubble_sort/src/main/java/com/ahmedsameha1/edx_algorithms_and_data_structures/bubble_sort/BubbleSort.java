98f60349907adc667edf020c3942753bc59c9691
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2019-02-27 17:54:16 +0200

    TDD bubble sort

---
+++ bubble_sort/src/main/java/com/ahmedsameha1/edx_algorithms_and_data_structures/bubble_sort/BubbleSort.java
@@ -0,0 +1,19 @@
+package com.ahmedsameha1.edx_algorithms_and_data_structures.bubble_sort;
+
+public class BubbleSort {
+    public int[] sort(int[] arr) {
+        boolean swap_done = true;
+        while (swap_done) {
+            swap_done = false;
+            for ( int i = 0; i < arr.length - 1; i++ ) {
+                if ( arr[i + 1] < arr[i] ) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swap_done = true;
+                }
+            }
+        }
+        return arr;
+    }
+}
---
+++ bubble_sort/src/test/java/com/ahmedsameha1/edx_algorithms_and_data_structures/bubble_sort/TestBubbleSort.java
@@ -0,0 +1,24 @@
+package com.ahmedsameha1.edx_algorithms_and_data_structures.bubble_sort;
+
+// Copied from: https://junit.org/junit5/docs/5.0.1/api/org/junit/jupiter/api/Assertions.html
+import static org.junit.jupiter.api.Assertions.*;
+// Copied from: https://junit.org/junit5/docs/current/user-guide/
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestBubbleSort {
+    private BubbleSort bubbleSort;
+
+    @BeforeEach
+    public void setup() {
+        bubbleSort = new BubbleSort();
+    }
+
+    @Test
+    public void test() {
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, bubbleSort.sort(new int[]{5, 4, 3, 2, 1}));
+        assertArrayEquals(new int[] {1, 2, 3, 4}, bubbleSort.sort(new int[] { 4, 3, 2, 1}));
+        assertArrayEquals(new int[] {-1, 1, 2, 3, 4}, bubbleSort.sort(new int[] { 4, 3, 2, 1, -1}));
+        assertArrayEquals(new int[] {-1, 15, 15, 52, 73, 104}, bubbleSort.sort(new int[] { 15, 73, 104, 15, 52, -1}));
+    }
+}
