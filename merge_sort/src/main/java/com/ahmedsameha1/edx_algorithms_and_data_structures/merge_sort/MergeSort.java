059a6e31340b4230be3373bef1df1254e3c9071f
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2019-03-01 14:52:07 +0200

    Creat TDD merge sort file structure

---
+++ merge_sort/src/main/java/com/ahmedsameha1/edx_algorithms_and_data_structures/merge_sort/MergeSort.java
@@ -0,0 +1,7 @@
+package com.ahmedsameha1.edx_algorithms_and_data_structures.merge_sort;
+
+public class MergeSort {
+    public int[] sort(int[] arr) {
+        return new int[]{};
+    }
+}
---
+++ merge_sort/src/test/java/com/ahmedsameha1/edx_algorithms_and_data_structures/merge_sort/TestMergeSort.java
@@ -0,0 +1,24 @@
+package com.ahmedsameha1.edx_algorithms_and_data_structures.merge_sort;
+
+// Copied from: https://junit.org/junit5/docs/5.0.1/api/org/junit/jupiter/api/Assertions.html
+import static org.junit.jupiter.api.Assertions.*;
+// Copied from: https://junit.org/junit5/docs/current/user-guide/
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestMergeSort {
+    private MergeSort mergeSort;
+
+    @BeforeEach
+    public void setup() {
+        mergeSort = new MergeSort();
+    }
+
+    @Test
+    public void test() {
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, mergeSort.sort(new int[]{5, 4, 3, 2, 1}));
+        assertArrayEquals(new int[] {1, 2, 3, 4}, mergeSort.sort(new int[] { 4, 3, 2, 1}));
+        assertArrayEquals(new int[] {-1, 1, 2, 3, 4}, mergeSort.sort(new int[] { 4, 3, 2, 1, -1}));
+        assertArrayEquals(new int[] {-1, 15, 15, 52, 73, 104}, mergeSort.sort(new int[] { 15, 73, 104, 15, 52, -1}));
+    }
+}
