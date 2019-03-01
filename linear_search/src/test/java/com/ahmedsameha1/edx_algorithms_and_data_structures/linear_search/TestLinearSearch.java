7529f25a6d99681d71b3b317191fce69c4a5027e
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2019-03-01 14:01:05 +0200

    Use assertTrue and assertFalse in linear search

--- linear_search/src/test/java/com/ahmedsameha1/edx_algorithms_and_data_structures/linear_search/TestLinearSearch.java
+++ linear_search/src/test/java/com/ahmedsameha1/edx_algorithms_and_data_structures/linear_search/TestLinearSearch.java
@@ -16,7 +16,7 @@ public class TestLinearSearch {
 
     @Test
     public void test() {
-        assertEquals(true, linearSearch.search(new int[] { 1, 3, 5 }, 5));
-        assertEquals(false, linearSearch.search(new int[] { 1, 3, 5}, 7));
+        assertTrue(linearSearch.search(new int[] { 1, 3, 5 }, 5));
+        assertFalse(linearSearch.search(new int[] { 1, 3, 5}, 7));
     }
 }
