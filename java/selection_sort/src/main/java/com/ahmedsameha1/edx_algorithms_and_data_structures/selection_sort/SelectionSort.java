0c5ffff31d64c7d0755de9e58725ea0c7e570e8d
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2019-03-08 20:59:59 +0200

    Remove the unnecessary min variable from the selection sort algorithm Java implementation

--- java/selection_sort/src/main/java/com/ahmedsameha1/edx_algorithms_and_data_structures/selection_sort/SelectionSort.java
+++ java/selection_sort/src/main/java/com/ahmedsameha1/edx_algorithms_and_data_structures/selection_sort/SelectionSort.java
@@ -5,11 +5,10 @@ import java.util.Arrays;
 public class SelectionSort {
     public int[] sort(int[] intArr) {
         for (int i = 0; i < intArr.length; i++) {
-            int min = i;
             for (int j = i + 1; j < intArr.length; j++) {
-                if ( intArr[j] < intArr[min] ) {
-                    int tmp = intArr[min];
-                    intArr[min] = intArr[j];
+                if ( intArr[j] < intArr[i] ) {
+                    int tmp = intArr[i];
+                    intArr[i] = intArr[j];
                     intArr[j] = tmp;
                 }
             }
