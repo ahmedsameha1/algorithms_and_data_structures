5c1ac453cc657d7433fd1f13a65c2cf764383cc3
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2019-03-08 20:55:16 +0200

    Add selection sort algorithm JavaScript implementation

---
+++ javascript/selection_sort/src/selection_sort.js
@@ -0,0 +1,14 @@
+const sort = (arr) => {
+    for ( let i = 0; i < arr.length; i++ ) {
+        for ( let j = i + 1; j < arr.length; j++ ) {
+            if ( arr[j] < arr[i] ) {
+                let temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+exports.sort = sort;
