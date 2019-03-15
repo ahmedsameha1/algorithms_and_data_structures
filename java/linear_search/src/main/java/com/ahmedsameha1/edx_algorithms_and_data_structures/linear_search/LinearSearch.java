f1dd9d7a9a9cf51132e5b2b16bc34b2ffdf47619
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2019-03-15 13:12:14 +0200

    Change linear search algorithm Java implementation to use for each loop

--- java/linear_search/src/main/java/com/ahmedsameha1/edx_algorithms_and_data_structures/linear_search/LinearSearch.java
+++ java/linear_search/src/main/java/com/ahmedsameha1/edx_algorithms_and_data_structures/linear_search/LinearSearch.java
@@ -2,8 +2,8 @@ package com.ahmedsameha1.edx_algorithms_and_data_structures.linear_search;
 
 public class LinearSearch {
     public boolean search(int num, int... arr) {
-        for ( int i = 0; i < arr.length; i++ ) {
-            if ( arr[i] == num ) {
+        for ( int i: arr ) {
+            if ( i == num ) {
                 return true;
             }
         }
